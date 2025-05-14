a31571426b67314a670a977f676d60010836b17b
elishaadamu <elishadamu97@gmail.com>
2025-05-14 14:52:16 +0100

    work on the home page

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,26 +1,32 @@
 // src/pages/Home.jsx
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 import Logo from "./assets/logo-ay.png";
 import Svg from "./assets/bg.svg";
-import Background from "./assets/bg-form.jpg";
-import { MdEmail } from "react-icons/md";
-import { TbLockPassword } from "react-icons/tb";
+import { TbEye, TbEyeOff } from "react-icons/tb"; // Eye icons for toggle
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
     <motion.div
-      className="min-h-screen flex items-center justify-center bg-gray-100  px-4"
+      className="min-h-screen flex items-center justify-center relative bg-gray-100  px-4"
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: -100 }}
       transition={{ duration: 0.5 }}
     >
-      <div className="bg-white my-8  p-8 rounded shadow-md w-full max-w-md">
-        <div className="bg-form z-20 absolute">
-          <img src={Svg} alt="" />
-        </div>
+      <div className="bg-form-1 z-2 absolute">
+        <img src={Svg} alt="" />
+      </div>
+      <div className="bg-white my-8 z-10 p-8  rounded shadow-md w-full max-w-md">
         <div className=" my-5 mx-auto w-40 max-w-full">
           <NavLink
             exact="true"
@@ -123,15 +129,36 @@ const Signup = () => {
               PASSWORD
             </label>
           </p>
-
-          <input
-            type="password"
-            className="pl-5 py-2 border border-gray-500 rounded w-full h-[50px]"
-            placeholder="Password"
-            required
-            name="password"
-            id="Password"
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"} // Toggle between text and password
+              className="pl-5 py-2 border border-gray-500 rounded w-full h-[50px]"
+              placeholder="Password"
+              required
+              name="password"
+              id="Password"
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="absolute right-3 top-4 cursor-pointer text-gray-500 "
+            >
+              {showPassword ? <TbEyeOff size={20} /> : <TbEye size={20} />}
+            </button>
+          </div>
+          <div className="flex items-center space-x-2 mt-4">
+            <input
+              type="checkbox"
+              id="terms"
+              name="terms"
+              className="w-4 h-4 cursor-pointer  text-green-500 border border-gray-100 rounded focus:ring-green-500 focus:ring-1"
+            />
+            <label htmlFor="remember" className="text-sm text-gray-700">
+              <a href="#" className="text-amber-600">
+                Accept terms and condition
+              </a>
+            </label>
+          </div>
           <input
             type="submit"
             value="Signup"
@@ -142,46 +169,42 @@ const Signup = () => {
         <p className="text-gray-400 text-center text-xl my-5">or</p>
         <div className="flex flex-col md:flex-row gap-3">
           {/* Google Button */}
-          <button className="border border-gray-500 hover:bg-gray-300 rounded h-10 flex items-center justify-center w-full">
+          <a
+            href="#"
+            className="border border-gray-500 hover:bg-gray-50 cursor-pointer rounded h-10 flex items-center justify-center w-full"
+          >
             <div className="flex gap-4 items-center">
               {/* SVG icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 48 48"
-                width="24px"
-                height="24px"
-              >
-                ...
-              </svg>
+              <FaGoogle className="text-[#DB4437]" />
               <span className="font-bold">Google</span>
             </div>
-          </button>
+          </a>
 
           {/* Facebook Button */}
-          <button className="border border-gray-500 hover:bg-gray-300 rounded h-10 flex items-center justify-center w-full">
+          <a
+            href="#"
+            className="border border-gray-500 hover:bg-gray-50 cursor-pointer rounded h-10 flex items-center justify-center w-full"
+          >
             <div className="flex gap-4 items-center">
               {/* SVG icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 48 48"
-                width="24px"
-                height="24px"
-              >
-                ...
-              </svg>
+              <FaFacebookF className="text-[#1877F2]" />
+
               <span className="font-bold">Facebook</span>
             </div>
-          </button>
+          </a>
         </div>
 
         <p className="mt-5 text-center">
           <span className="text-gray-400 text-[16px]">
             Already have an account?
           </span>{" "}
-          <NavLink to="/login" className="text-blue-700 text-[16px] font-bold">
-            Log In
+          <NavLink to="/login" className="text-amber-600 text-[16px] font-bold">
+            Log In to your Account
           </NavLink>
         </p>
+      </div>
+      <div className="bg-form-2 absolute">
+        <img src={Svg} alt="" className="w-full" />
       </div>
     </motion.div>
   );
