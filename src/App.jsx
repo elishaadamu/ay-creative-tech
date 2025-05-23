0fe0930358b84e9dd25a27e237a2ccdf775928e0
elishaadamu <elishadamu97@gmail.com>
2025-05-23 16:48:46 +0100

    add create account

--- src/App.jsx
+++ src/App.jsx
@@ -37,23 +37,23 @@ const App = () => {
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <svg
-          className="animate-spin h-12 w-12 text-amber-500"
+          className="animate-spin h-16 w-16 text-amber-500 drop-shadow-lg"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
-          viewBox="0 0 24 24"
+          viewBox="0 0 50 50"
         >
           <circle
             className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
+            cx="25"
+            cy="25"
+            r="20"
             stroke="currentColor"
-            strokeWidth="4"
+            strokeWidth="6"
           ></circle>
           <path
             className="opacity-75"
             fill="currentColor"
-            d="M4 12a8 8 0 018-8v8z"
+            d="M25 5a20 20 0 0 1 20 20h-6a14 14 0 0 0-14-14V5z"
           ></path>
         </svg>
       </div>
